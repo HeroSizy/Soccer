0ffc71954e087206c4cbf9d0670e7742604ca0af
Sizy <scar.syler@gmail.com>
2017-03-08 19:39:19 +0800

    Player Acceleration

---
+++ Player/calAcc.m
@@ -0,0 +1,5 @@
+function acceleration = calAcc(a,b)
+    deltaS = sqrt((a(4)-b(4))^2 + (a(5)-b(5))^2);
+    
+    acceleration = 2*deltaS/(0.04)^2;
+end
---
+++ Player/player.m
@@ -0,0 +1,32 @@
+cd('/Users/Scar/GitHub/Player/playerAcc/');
+filename = 'DetectedPlayers.xlsx';
+sheet = 1;
+xlRange = 'A1:E55723';
+
+allData = xlsread(filename,sheet,xlRange);
+[sizeRow, sizeCol] = size(allData);
+
+ids = unique(allData(:,3));
+
+allData(:,[4,5]) = allData(:,[4,5]) / 100;
+ for i = 1:size(ids)
+
+    playerid = ids(i);
+%     playerid = 5701;
+    playerdata = [];
+
+    for j = 1 : sizeRow
+        if(allData(j,3) == playerid)
+           playerdata = [playerdata;allData(j, 1:5) ];
+        end
+    end
+    playerAcc = [];
+    for k = 1: (size(playerdata,1)-1)
+        playerAcc = [playerAcc ,calAcc(playerdata(k,:), playerdata(k+1,:))];
+    end
+    playerdata(1,:) = [];  
+    if ~isempty(playerdata)
+    saveas(plot(playerdata(:,2), playerAcc),num2str(playerid),'jpg');
+%     saveas(plot(smooth(playerdata(:,2)), playerAcc),[num2str(playerid) 'smooth'],'jpg');
+    end
+ end
