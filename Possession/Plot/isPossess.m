0ede4ba0e3be1faf2608f94fe335aa8bf90d23ae
Sizy <scar.syler@gmail.com>
2017-04-05 11:07:58 +0800

    possession explained

--- Possession/Plot/isPossess.m
+++ Possession/Plot/isPossess.m
@@ -1,4 +1,5 @@
 function possessed = isPossess(ball,player)
+    %calculate distance between ball and player and return the value
     distance = sqrt((ball(3)-player(4))^2 + (ball(4)-player(5))^2); 
     possessed = [player(3),distance];
 end
